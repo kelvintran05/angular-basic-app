4f93aefdf94e4df582895cff7aeac9862da35307
Tri Tran <triit95@gmail.com>
2018-12-28 15:13:31 +0700

    last changed

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Recipe } from './../recipe.model';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new  Recipe('A Test Recipe', 'This is simply a test',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg'),
+    new  Recipe('A Test Recipe', 'This is simply a test',
+    'https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Recipe_logo.jpeg/600px-Recipe_logo.jpeg')
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
